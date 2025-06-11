480ffb4d952642b7e0ef5715cb0ca568a4df9f6c
unknown <paulsong29@gmail.com>
2025-06-10 20:20:53 -0400

    added basename in broswer router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Projects from "./components/Projects/Projects";
 import About from "./components/About/About";
 import Home from "./components/Home/Home";
@@ -13,14 +13,12 @@ function App() {
   return (
     <>
       <ParticlesBackground />
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects projects={PROJECTS} />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects projects={PROJECTS} />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <NavBar />
     </>
   );
