76ec99d04e8cbc5399b726a7123c00ed8a81c5e2
unknown <paulsong29@gmail.com>
2025-06-11 13:47:36 -0400

    cleaned up about me; will add more

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,19 +8,15 @@ function AboutMe() {
       </h1>
       <div className="bg-black/50 backdrop-blur-sm rounded-lg p-8 shadow-lg">
         <p className="text-lg leading-relaxed">
-          My name is <span className="font-bold">Paul Song</span> and I am from
-          Milledgeville, Georgia. I earned a{" "}
-          <span className="font-bold">B.S. Ed in Exercise Science</span> from
-          UGA and a Full-Stack Web Development Certificate from Georgia Tech.
-          Currently, I am pursuing a{" "}
-          <span className="font-bold">B.S. in Computer Science</span>
-          to deepen my understanding of computing fundamentals.
-          <br />
-          <br />I have four years of professional work experience at{" "}
-          <span className="font-bold">Emory Healthcare</span> where I served as
-          a Patient Access Scheduler and helped manage an inpatient rehab
-          facility. My passion lies in solving complex problems with technology,
-          which is why I am dedicated to becoming a full-time software engineer.
+          My name is Paul Song and I am from Milledgeville, Georgia. I earned a
+          B.S. Ed in Exercise Science from UGA and a Full-Stack Web Development
+          Certificate from Georgia Tech. Currently, I am pursuing a B.S. in
+          Computer Science to deepen my understanding of computing fundamentals.
+          I have four years of professional work experience at Emory Healthcare
+          where I served as a Patient Access Scheduler and helped manage an
+          inpatient rehab facility. My passion lies in solving complex problems
+          with technology, which is why I am dedicated to becoming a full-time
+          software engineer.
         </p>
       </div>
     </section>
